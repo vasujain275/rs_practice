528d0fa6c90c909fc5a5cea7fe2dd93a02cdfed0
vasujain275 <vasujain275@gmail.com>
2024-05-27 22:41:34 +0530

    feat: completed temp_conveter

--- 01_temp_converter/src/main.rs
+++ 01_temp_converter/src/main.rs
@@ -1,22 +1,29 @@
 use std::io;
 
-enum Format {
-    C(i32),
-    F(i32),
-}
-
 fn main() {
     println!("Welcome to Blazingly fast Calculator!");
     println!("Enter the Unit of Temperature (C/F): ");
-    let mut opt = String::new(); // Defaults to Celcius
-    match io::stdin().read_line(&mut opt) {
-        Ok(..) => {
-            if opt.to_uppercase().eq("C") {
-                println!("YES")
-            }
-        }
-        Err(error) => {
-            println!("error : {error}")
-        }
+    let mut unit = String::new();
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut unit)
+        .expect("Error reading the unit");
+
+    println!("Enter the value: ");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Error reading the value");
+
+    let input: f32 = input
+        .trim()
+        .parse()
+        .expect("Error Parsing the input to integer");
+
+    if unit.trim().to_uppercase().eq("C") {
+        println!("Converted is - {}F", ((input * 1.8) + 32.0));
+    }
+
+    if unit.trim().to_uppercase().eq("F") {
+        println!("Converted is - {} C", ((input - 32.0) * 0.56));
     }
 }
